1b106f48009ae94627b308a4d6969d7c7da5fcd6
yanjuehau <landonhyj@gmail.com>
2020-04-24 18:14:02 +0000

    created the server.js , port works (using the db.js in the models file)

---
+++ server.js
@@ -0,0 +1,25 @@
+require('./models/database');
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const book_controller = require('./controllers/book_controller');
+
+var app = express();
+var port = process.env.port || 3000;
+
+app.set('views', path.join(__dirname, '/views'));
+app.engine('hbs', exphbs({extname: 'hbs', 
+                defaultLayout: 'mainLayout', 
+                    layoutsDir: ++__dirname + '/views/layout/' }));
+app.set('view engine', 'hbs');
+
+
+app.listen(port, function(err){
+    console.log("Listening on Port" + port)
+
+
+});
+
+app.use('/book',book_controller );
