cfcc4a1ea0e90d36c02ca79d05774ad7d7c38522
yanjuehau <landonhyj@gmail.com>
2020-04-25 16:11:30 +0000

    render the hbs file to the client and created post method & insert function

--- controllers/book_controller.js
+++ controllers/book_controller.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Book = mongoose.model('Book');
 
 router.get('/',(req,res) => {
     res.render("book/addAndEdit",{
@@ -8,5 +10,32 @@ router.get('/',(req,res) => {
     });
 
 });
+
+router.post('/',(req,res) => {
+    insertNewBook(req,res);
+});
+
+
+function insertNewBook(req, res){
+    var book = new Book();
+    book.titleName = req.body.titleName;
+    book.author = req.body.author;
+    book.year = req.body.year;
+    book.price = req.body.price;
+    book.save((err, doc) => {
+        if (!err)
+            res.redirect('book/list');
+        else{
+            console.log('Error when insertion : ' + err);
+        }
+    });
+}
+
+router.get('/list',(req,res) => {
+    res.json('from list');
+
+
+});
+
 
 module.exports = router ;
