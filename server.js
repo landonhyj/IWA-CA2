dc9b0cfaba70c4942a85eab7688fd2044e834b15
yanjuehau <landonhyj@gmail.com>
2020-04-25 16:14:01 +0000

    alternate and added the body parser with json format

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ require('./models/database');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
 
 const book_controller = require('./controllers/book_controller');
 
-var app = express();
 var port = process.env.port || 3000;
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
+app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views'));
 app.engine('hbs', exphbs({ extname: 'hbs', 
                 defaultLayout: 'mainLayout', 
